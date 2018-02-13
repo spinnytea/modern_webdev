5d72bf06a3375f18dbe6cedd78bbbbb55cf9c641
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2018-02-12 19:09:29 -0500

    start of vendor

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,45 @@
 'use strict';
+const del = require('del');
 const gulp = require('gulp');
+const merge = require('merge-stream');
+const path = require('path');
+
 
 // build config
 
-const dist = 'dist';
-const resources = Object.freeze({
-	static: [ 'static/**/*' ],
+const dist = Object.freeze({
+	root: 'dist',
+	vendor: 'dist/vendor',
 });
+const resources_static = 'static/**/*';
+const vendor = [
+	[ 'jquery', 'node_modules/jquery/dist/jquery.min.js' ],
+	[ 'bootstrap', 'node_modules/bootstrap/dist/**/*' ],
+];
+
 
 // build tasks
 
-gulp.task('build-static', function() {
-	return gulp.src(resources.static)
-		.pipe(gulp.dest(dist));
+gulp.task('build:static', function() {
+	return gulp.src(resources_static)
+		.pipe(gulp.dest(dist.root));
 });
 
-gulp.task('build', ['build-static']);
+gulp.task('build:vendor', function() {
+	return merge(vendor.map(function([dest, src]) {
+		return gulp.src(src).pipe(gulp.dest(path.join(dist.vendor, dest)));
+	}));
+});
+
+gulp.task('build', [ 'build:static', 'build:vendor' ]);
+
+
+// clean up workspace
+
+gulp.task('clean', function() {
+	return del(dist.root);
+});
+
+gulp.task('clean:vendor', function() {
+	return del(dist.vendor);
+});
