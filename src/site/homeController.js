1aab862b6012d643c42cf22ccb4772ee396ca2d3
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2018-02-21 19:12:56 -0500

    tweaks to home page

--- src/site/homeController.js
+++ src/site/homeController.js
@@ -1,12 +1,13 @@
 define(['lodash'], function (_) {
 	return [
-		'$scope', 'po_ke_type.site.settings', 'po_ke_type.pokedex.pokedexService',
+		'$scope', 'po_ke_type.site.settings', 'po_ke_type.pokedex.team.factory', 'po_ke_type.pokedex.pokedexService',
 		HomeController,
 	];
 
-	function HomeController($scope, settings, pokedexService) {
+	function HomeController($scope, settings, team, pokedexService) {
 		$scope.themeCount = settings.themes.length;
 		$scope.pokedexCount = pokedexService.pokedex.length;
 		$scope.typeCount = _.size(pokedexService.types);
+		$scope.team = team;
 	}
 });
