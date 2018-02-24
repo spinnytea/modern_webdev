cbe769af4c6ded1a96f002f98ed7c04348d6e91e
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2018-02-24 10:17:02 -0500

    remove pokemonService

--- src/pokedex/pokedexModule.js
+++ src/pokedex/pokedexModule.js
@@ -4,7 +4,6 @@ define([
 	'./dexGenFilter',
 	'./pokedexController',
 	'./pokedexFactory',
-	'./pokedexService',
 	'./pokemonController',
 	'./pokemonPillDirective',
 	'./teamController',
@@ -14,7 +13,6 @@ define([
 		dexGenFilter,
 		pokedexController,
 		pokedexFactory,
-		pokedexService,
 		pokemonController,
 		pokemonPillDirective,
 		teamController,
@@ -25,7 +23,6 @@ define([
 	pokedexModule.filter('dexGen', dexGenFilter);
 	pokedexModule.controller('po_ke_type.pokedex.controller', pokedexController);
 	pokedexModule.factory('po_ke_type.pokedex.factory', pokedexFactory);
-	pokedexModule.service('po_ke_type.pokedex.pokedexService', pokedexService);
 	pokedexModule.controller('po_ke_type.pokedex.pokemon.controller', pokemonController);
 	pokedexModule.directive('pokemonPill', pokemonPillDirective);
 	pokedexModule.controller('po_ke_type.pokedex.team.controller', teamController);
