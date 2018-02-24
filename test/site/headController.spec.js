0a68428e2d2faf779de544c0f95fd2d0269c2ca4
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2018-02-24 18:26:31 -0500

    head/home controller spec

---
+++ test/site/headController.spec.js
@@ -0,0 +1,28 @@
+define([
+	'angular',
+	'src/site/siteModule',
+	'angular-mocks',
+], function (angular, siteModule) {
+	return describe('Head Controller', function () {
+		var settingsFactory = {};
+		beforeEach(angular.mock.module(siteModule.name, function ($provide) {
+			$provide.value('po_ke_type.site.settings.factory', settingsFactory);
+		}));
+
+		describe('controller', function () {
+			var $scope;
+			beforeEach(angular.mock.inject(['$controller', function ($controller) {
+				$scope = {};
+				$controller('po_ke_type.site.head.controller', {
+					'$scope': $scope,
+				});
+			}]));
+
+			it('init', function () {
+				expect($scope.settings).toBe(settingsFactory);
+			});
+		}); // end controller
+
+		it('template'); // end template
+	}); // end Head Controller
+});
