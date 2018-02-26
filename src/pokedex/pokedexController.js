a2867794af71313f35945356838125fd718c156c
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2018-02-25 22:10:26 -0500

    clear out data-

--- src/pokedex/pokedexController.js
+++ src/pokedex/pokedexController.js
@@ -5,6 +5,8 @@ define([], function () {
 	];
 
 	// TODO save filter string in local storage
+	// TODO sort order
+	// TODO change limits (multiples of 6)
 	function PokedexController($scope, pokedex) {
 		$scope.dex = pokedex.list;
 		$scope.nested = { filter: '', limit: 20 };
