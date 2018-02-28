a0849a1f73c1b881c21a675741f799b8c24548a6
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2018-02-28 02:36:09 -0500

    ... since we have a setup.js

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,17 @@
+/* global angular: false */
 'use strict';
-beforeEach(function () {
+
+// setup a module to deal with awkward dependencies
+beforeAll(function () {
 	var module = angular.module('setup.js', []);
 	module.value('po_ke_type.utils.tours.factory', {
 		register: angular.noop,
 		start: angular.noop,
 	});
 });
+
+// jasmine uses wierd syntax... .skip and .only is cleaner
+it.skip = xit;
+it.only = fit;
+describe.skip = xdescribe;
+describe.only = fdescribe;
